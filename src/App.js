7735462a30bd0650b7123660d18f7552fd9d2444
root <mtrhoades0727@gmail.com>
2022-04-22 06:49:11 -1000

    added loader spinner, react lazy loading for components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import { TailSpin } from 'react-loader-spinner'
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+
+
   return (
     <div className="App">
+      <div>
+        <Suspense fallback={<TailSpin
+          type="Bars"
+          color="#FF00FF"
+          height={200}
+          width={200}
+          style={{margin: "0 auto"}}/>}>
+            <h1>Hello World</h1>
+          </Suspense>
+      </div>
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<h1>Loading...</h1>}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
